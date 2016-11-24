64063bc3853e57bcb7eb69679ef98f02d8f0a805
Schyler Hosler <schylerhosler@gmail.com>
2016-11-23 19:46:36 -0500

    Reverting Quiz form elements to single column

--- src/components/lesson.js
+++ src/components/lesson.js
@@ -10,7 +10,7 @@ export default function Lesson(props) {
     <div className="lesson-div">
       <h2>Lesson I</h2>
       <p>To start, we'll need to have an understanding of one of the Japanese language's basic writing systems. Your first introduction to this will be <em>hiragana</em>. While we will go over each one individually, the initial list of <em>hiragana</em> characters we will cover is as follows:</p>
-      <div className="center-align">
+      <div className="center-kana">
         <table className="hiragana">
           <tr>
             <td>　</td>
@@ -81,11 +81,11 @@ export default function Lesson(props) {
       </div>
       <p>We'll begin with focusing on the the first 5 <em>hiragana</em> characters from the top right, moving down. (Please note that the use of "hook brackets" (「　」) around each character is the Japanese equivalent of quotation marks.):</p>
       <div className="center-align">
-        <p>「あ」 is the vowel "a" (pronounced "AH") sound.</p>
-        <p>「い」 is the vowel "i" (pronounced "EE") sound.</p>
-        <p>「う」 is the vowel "u" (pronounced "OO") sound.</p>
-        <p>「え」 is the vowel "e" (pronounced "EH") sound.</p>
-        <p>「お」 is the vowel "o" (pronounced "OH") sound.</p>
+        <p>「あ」 is the vowel "a" (pronounced "AH").</p>
+        <p>「い」 is the vowel "i" (pronounced "EE").</p>
+        <p>「う」 is the vowel "u" (pronounced "OO").</p>
+        <p>「え」 is the vowel "e" (pronounced "EH").</p>
+        <p>「お」 is the vowel "o" (pronounced "OH").</p>
       </div>
       <Quiz />
     </div>
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -12,36 +12,61 @@ export default function Quiz(props) {
         <h3>Quiz Time!</h3>
         Please write the romanized equivalent of the following <em>hiragana</em> characters (peeking is okay, but try to say them out loud!):
       </div>
-      <form className="kana-quiz-form">
-        <div className="kana-quiz-text">
-          <div className="center-align">
-          「あ」
+      <form>
+        <div className="center-align">
+        <table className="kana-quiz-table">
+          <tr>
+            <td className="kana-quiz-text">「あ」</td>
+            <td><Input /></td>
+            {/* <td className="kana-quiz-text">Correct!</td> */}
+          </tr>
+          <tr>
+            <td className="kana-quiz-text">「い」</td>
+            <td><Input /></td>
+          </tr>
+          <tr>
+            <td className="kana-quiz-text">「う」</td>
+            <td><Input /></td>
+          </tr>
+          <tr>
+            <td className="kana-quiz-text">「え」</td>
+            <td><Input /></td>
+          </tr>
+          <tr>
+            <td className="kana-quiz-text">「お」</td>
+            <td><Input /></td>
+          </tr>
+          {/* <div className="kana-quiz-text">
+            「あ」
           </div>
-          <Input />
-        </div>
-        <div className="kana-quiz-text">
-          <div className="center-align">
-          「い」
+          <div className="kana-quiz-input">
+            <Input />
           </div>
-          <Input />
-        </div>
-        <div className="kana-quiz-text">
-          <div className="center-align">
-          「い」
+          <div className="kana-quiz-text">
+            「い」
           </div>
-          <Input />
-        </div>
-        <div className="kana-quiz-text">
-          <div className="center-align">
-          「え」
+          <div className="kana-quiz-input">
+            <Input />
           </div>
-          <Input />
-        </div>
-        <div className="kana-quiz-text">
-          <div className="center-align">
-          「お」
+          <div className="kana-quiz-text">
+            「う」
           </div>
-          <Input />
+          <div className="kana-quiz-input">
+            <Input />
+          </div>
+          <div className="kana-quiz-text">
+            「え」
+          </div>
+          <div className="kana-quiz-input">
+            <Input />
+          </div>
+          <div className="kana-quiz-text">
+            「お」
+          </div>
+          <div className="kana-quiz-input">
+            <Input />
+          </div> */}
+        </table>
         </div>
         <div className="center-btn">
           <button type="submit" className="next-lesson-btn"><span>Next Lesson</span></button>
