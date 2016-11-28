e5c723239883c62838119ea7d8b0f8135d4a1595
Schyler Hosler <schylerhosler@gmail.com>
2016-11-28 14:40:07 -0500

    Minor changes to hrefs for navbar links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,11 +15,11 @@ export default class Navbar extends React.Component {
         <nav>
           <ul>
             <li><span>Welcome to Polyglot</span></li>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li className="float-right"><a href="#signin">Sign In</a></li>
-            <li className="float-right"><a href="#signup">Sign Up</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li className="float-right"><a href="/signin">Sign In</a></li>
+            <li className="float-right"><a href="/signup">Sign Up</a></li>
           </ul>
         </nav>
         <Track />
--- src/components/track.js
+++ src/components/track.js
@@ -5,14 +5,23 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import Lesson from './lesson';
 
-export default function Track(props) {
-  return(
-    <div className="track-div">
-      <h2>
-        <a className="track-link" href="#japanese">Language Track: 日本語</a>
-      </h2>
-      <Lesson />
-      {/* {this.props.children} */}
-    </div>
-  )
+export default class Track extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      track: "Japanese",
+      lesson: "1"
+    }
+  }
+  render() {
+    return(
+      <div className="track-div">
+        <h2>
+          <a className="track-link" href="/japanese">Language Track: 日本語</a>
+        </h2>
+        <Lesson />
+        {/* {this.props.children} */}
+      </div>
+    )
+  }
 }
